c8e23106506f13f1098fa62c5ed0f7cf6fdc550f
Stephen Anderson and Nicholas Davies <dev+bendycode+nbdavies@bendyworks.com>
2016-05-07 12:26:55 -0500

    new rspec expect syntax

--- spec/models/set_list_spec.rb
+++ spec/models/set_list_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SetList, type: :model do
-  it { should validate_numericality_of(:target_length_in_seconds).is_greater_than_or_equal_to(0) }
-  it { should validate_presence_of :name }
-  it { should have_and_belong_to_many :songs }
+  it { expect(subject).to validate_numericality_of(:target_length_in_seconds).is_greater_than_or_equal_to(0) }
+  it { expect(subject).to validate_presence_of :name }
+  it { expect(subject).to have_and_belong_to_many :songs }
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  it {should validate_numericality_of(:length_in_seconds).is_greater_than_or_equal_to(0) }
-  it {should validate_presence_of :length_in_seconds}
-  it {should validate_presence_of :title}
-  it {should have_and_belong_to_many :set_lists}
+  it { expect(subject).to validate_numericality_of(:length_in_seconds).is_greater_than_or_equal_to(0) }
+  it { expect(subject).to validate_presence_of :length_in_seconds }
+  it { expect(subject).to validate_presence_of :title }
+  it { expect(subject).to have_and_belong_to_many :set_lists }
 end
